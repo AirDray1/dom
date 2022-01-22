43c85c3dd93e2a8cf7839c903217b64f3606d771
Andrey <belous@gmail.com>
2022-01-22 08:52:57 +0200

    add search and filter by price

--- Dom/hw/js/script.js
+++ Dom/hw/js/script.js
@@ -90,7 +90,7 @@ function addProduct({name, imgUrl, id, display, storage, faceId, touchId, price,
   <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"/>
 </svg> Корзина: ${currentCount}`;
 
-  a.addEventListener('click', (e) => { inStock > 0 ? counter() : alert('Данный товар отсутствует на складе')});
+  b.addEventListener('click', (e) => { inStock > 0 ? counter() : alert('Данный товар отсутствует на складе')});
 
   function makeCounter() {
     return function() {
@@ -125,39 +125,93 @@ function addProduct({name, imgUrl, id, display, storage, faceId, touchId, price,
   document.querySelector('.products').appendChild(a)
 }
 
-function filter({price}) {
-  let a =  document.createElement('div');
-  a.classList = 'row center-block'; 
-  let b = document.createElement('button');
-  b.innerText = 'Выбрать';
-  b.classList = 'btn btn-primary mt-auto';
+function filterInput() {
+  let div = document.createElement('div');
+  let buttonSelect = document.createElement('button');
+  let buttonReset = document.createElement('button');
 
-  a.innerHTML = `
-    <label for="customRange2">Фильтр цены:</label>
-    <div class="col-md-6 p-2">
-      <input type="text" class="form-control firNum">
-    </div>
-    <div class="col-md-6 p-2">
-      <input type="text" class="form-control secNum">
-    </div>`;
+  buttonSelect.classList = 'btn btn-primary mt-auto';  
+  buttonReset.classList = 'btn btn-primary mt-auto';  
+  div.classList = 'row center-block';
   
-  
-  
-  a.addEventListener('click', (e) => { value instanceof Number ? getProduct() : alert('Вы ввели некоректное число')});
+  buttonSelect.innerText = 'Выбрать';
+  buttonReset.innerText = 'Сбросить';
+  div.innerHTML = `
+  <label class="customlbl">Фильтр цены:</label>
+  <div class="col-md-6 p-2">
+    <input type="text" class="form-control minNum">
+  </div>
+  <div class="col-md-6 p-2">
+    <input type="text" class="form-control maxNum">
+  </div>`;
 
-  function createNewListOfProducts(price) {
-    let mapProduct = price.map(function(x){
-      return console.log(x);
-    })
+  div.append(buttonSelect);
+  div.append(buttonReset);
+  buttonSelect.addEventListener('click', filter, false);
+  buttonReset.addEventListener('click', reset, false);
+  document.querySelector('.filter').appendChild(div);
+
+  let min = document.querySelector(".minNum");
+  let max = document.querySelector(".maxNum");
+
+  function filter(){  
+    let minval = min.value;
+    let maxval = max.value;
+    let products = document.querySelector('.products');
+    products.innerHTML = ``;
+    let filter = items.filter(function(product) {
+      return product.price > minval && product.price < maxval;
+    });
+    for (i = 0; i < items.length; i++) {
+      addProduct(filter[i])
+    }
   }
 
-  let getProduct = createNewListOfProducts();
+  function reset(){  
+    let products = document.querySelector('.products');    
+    let minval = min.value;
+    let maxval = max.value;
+    products.innerHTML = ``;
 
-  
-  
-    a.append(b)
+    for (i = 0; i < items.length; i++) {
+      addProduct(items[i])
+    }
+  }
+}
 
-  document.querySelector('.filter').appendChild(a)
+function searchInput() {
+  let div = document.createElement('div');
+  let buttonSearch = document.createElement('button');
+
+  buttonSearch.classList = 'btn btn-primary';  
+  div.classList = 'row center-block';
+  
+  buttonSearch.innerText = 'Найти';
+  div.innerHTML = `
+  <label class="customlbl">Поиск товара:</label>
+  <div class="col-md-12 p-2">
+    <input type="text" class="form-control searchInput">
+  </div>
+  `;
+
+  div.append(buttonSearch);
+  buttonSearch.addEventListener('click',search ,false);
+  document.querySelector('.search').appendChild(div);
+
+  let request = document.querySelector(".searchInput");
+
+  function search(){  
+    let searchProduct = request.value;
+    let products = document.querySelector('.products');
+    products.innerHTML = ``;
+    let search = items.filter(function(product) {
+        return product.name == searchProduct;
+    });
+    console.log(search);
+    for (i = 0; i < items.length; i++) {
+      addProduct(search[i])
+    }
+  }
 }
 
 let currentCount = [];
@@ -166,4 +220,6 @@ for (i = 0; i < items.length; i++) {
   addProduct(items[i])
 }
 
-filter();
+searchInput()
+
+filterInput()
